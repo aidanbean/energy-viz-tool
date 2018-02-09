39c1c9fb50c7f0374846342e0b87647f5dbdb8e2
aTonyXiao <tony.xiao95@icloud.com>
2018-02-08 23:25:43 -0800

    change listen port & start script

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 import app from './app';
 import env from './config/env';
 
-const server = app.listen(env.port, () => {
+
+const server = app.listen(eprocess.env.PORT || 8080, () => {
   console.log(`${env.name} server is listening at port ${env.port}`);
 });
 
