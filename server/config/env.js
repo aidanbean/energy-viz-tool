12d421081eb0cd2c9ffe4540ecbad23041988ea9
aTonyXiao <tony.xiao95@icloud.com>
2018-02-09 00:06:28 -0800

    test listening

--- server/config/env.js
+++ server/config/env.js
@@ -7,15 +7,18 @@
 const env = {
   production: {
     name: 'production',
-    port: process.env.PORT || 4000
+    // port: process.env.PORT || 4000
+    port: 4000
   },
   dev: {
     name: 'dev',
-    port: process.env.PORT || 4000
+    // port: process.env.PORT || 4000
+    port: 4000
   },
   test: {
     name: 'test',
-    port: process.env.PORT || 4000
+    // port: process.env.PORT || 4000
+    port: 4000
   }
 };
 export default env[process.env.NODE_ENV];
--- server/index.js
+++ server/index.js
@@ -2,9 +2,9 @@ import app from './app';
 import env from './config/env';
 
 
-const server = app.listen(process.env.PORT || 8080, () => {
+const server = app.listen(process.env.PORT || 4000, () => {
   // console.log(`${env.name} server is listening at port ${env.port}`);
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  console.log("Express server listening");
 });
 
 export default server;
