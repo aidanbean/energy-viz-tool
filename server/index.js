8424944304abbe9339bd87c7052d345668e9a222
aTonyXiao <tony.xiao95@icloud.com>
2018-02-08 23:38:15 -0800

    listen port

--- server/index.js
+++ server/index.js
@@ -2,8 +2,9 @@ import app from './app';
 import env from './config/env';
 
 
-const server = app.listen(eprocess.env.PORT || 8080, () => {
-  console.log(`${env.name} server is listening at port ${env.port}`);
+const server = app.listen(process.env.PORT || 8080, () => {
+  // console.log(`${env.name} server is listening at port ${env.port}`);
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
 export default server;
