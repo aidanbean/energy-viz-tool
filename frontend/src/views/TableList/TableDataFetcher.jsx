698d3ba0a5477dab87bf6d922673e7a1e54e2e92
aTonyXiao <tony.xiao95@icloud.com>
2018-03-11 17:36:56 -0700

    implements react table(support search, filter, sorting)
    //TODO: change Table content accordingly (depends on clients)

---
+++ frontend/src/views/TableList/TableDataFetcher.jsx
@@ -0,0 +1,43 @@
+// implement data fetcher, depends on what client want to display in the table
+
+const buildingArray = ["Kemper", "ACAD", "Silo", "Dutton", "Miller", "Peter A. Rock", "MU", "Bainer", "Wellman", "Meyer", "Giedt"];
+const equipmentArray = ["CCV", "AHU", "HCV", "HVO"];
+
+
+const range = len => {
+    const arr = [];
+    for (let i = 0; i < len; i++) {
+        arr.push(i);
+    }
+    return arr;
+};
+
+
+const fakeData = () => {
+    const statusChance = Math.random();
+    var num = Math.floor((Math.random() * 11));
+    return {
+        building: buildingArray[num],
+        equipmentType: equipmentArray[Math.floor((Math.random() * 4))],
+        equipmentNumber: Math.floor(Math.random() * 30),
+        visits: Math.floor(Math.random() * 100),
+        progress: Math.floor(Math.random() * 100),
+        // status:
+        //     statusChance > 0.66
+        //         ? "relationship"
+        //         : statusChance > 0.33 ? "complicated" : "single"
+    };
+};
+
+function dataFetcher(len = 5553) {
+    return range(len).map(d => {
+        return {
+            ...fakeData(),
+            children: range(10).map(fakeData)
+        };
+    });
+}
+
+export default dataFetcher;
+
+
--- frontend/src/views/TableList/TableList.jsx
+++ frontend/src/views/TableList/TableList.jsx
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import { Grid, Row, Col, Table } from 'react-bootstrap';
 
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+import matchSorter from 'match-sorter';
+
 import Card from '../../components/Card/Card.jsx';
-import {thArray, tdArray} from '../../variables/Variables.jsx';
+
+import dataFetcher from './TableDataFetcher.jsx';
 
 class TableList extends Component {
+    constructor() {
+        super();
+        this.state = {
+            data: dataFetcher()
+        };
+    }
 
     render() {
+        const { data } = this.state;
         return (
             <div className="content">
                 <Grid fluid>
@@ -17,77 +29,118 @@ class TableList extends Component {
                                 category="AHU"
                                 ctTableFullWidth ctTableResponsive
                                 content={
-                                    <Table striped hover>
-                                        <thead>
-                                            <tr>
+                                    <ReactTable
+                                    data={data}
+                                    filterable
+                                    defaultFilterMethod={(filter, row) =>
+                                        String(row[filter.id]).toLocaleLowerCase() === filter.value.toLocaleLowerCase()}
+                                    columns={[
+                                        {
+                                            // Header: "Name",
+                                            columns: [
                                                 {
-                                                    thArray.map((prop, key) => {
-                                                        return (
-                                                        <th  key={key}>{prop}</th>
-                                                        );
-                                                    })
+                                                    Header: "Building",
+                                                    accessor: "building",
+                                                    filterMethod: (filter, row) =>
+                                                        row[filter.id].startsWith(filter.value) ||
+                                                        row[filter.id].endsWith(filter.value)
+                                                },
+                                                {
+                                                    Header: "Equipment Type",
+                                                    id: "equipmentType",
+                                                    accessor: d => d.equipmentType,
+                                                    filterMethod: (filter, rows) =>
+                                                        matchSorter(rows, filter.value, { keys: ["equipmentType"] }),
+                                                    filterAll: true
                                                 }
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            {
-                                                tdArray.map((prop,key) => {
-                                                    return (
-                                                        <tr key={key}>{
-                                                            prop.map((prop,key)=> {
-                                                                return (
-                                                                    <td  key={key}>{prop}</td>
-                                                                );
-                                                            })
-                                                        }</tr>
-                                                    )
-                                                })
-                                            }
-                                        </tbody>
-                                    </Table>
+                                            ]
+                                        },
+                                        {
+                                            // Header: "Info",
+                                            columns: [
+                                                {
+                                                    Header: "Equipment Number",
+                                                    accessor: "equipmentNumber"
+                                                },
+                                                {
+                                                    Header: "Sensor Type",
+                                                    accessor: "SensorType",
+                                                    id: "over",
+                                                    Cell: ({ value }) => (value >= 21 ? "Yes" : "No"),
+                                                    filterMethod: (filter, row) => {
+                                                        if (filter.value === "all") {
+                                                            return true;
+                                                        }
+                                                        if (filter.value === "true") {
+                                                            return row[filter.id] >= 21;
+                                                        }
+                                                        return row[filter.id] < 21;
+                                                    },
+                                                    Filter: ({ filter, onChange }) =>
+                                                        <select
+                                                            onChange={event => onChange(event.target.value)}
+                                                            style={{ width: "100%" }}
+                                                            value={filter ? filter.value : "all"}
+                                                        >
+                                                            <option value="all">Show All</option>
+                                                            <option value="true">type1</option>
+                                                            <option value="false">type2</option>
+                                                        </select>
+                                                }
+                                            ]
+                                        }
+                                    ]}
+                                    defaultPageSize={10}
+                                    className="-striped -highlight"
+                                    />
+
+
                                 }
                             />
+
+
+
                         </Col>
 
 
-                        <Col md={12}>
-                            <Card
-                                plain
-                                title="Air Handler Unit Data"
-                                category="AHU Data"
-                                ctTableFullWidth ctTableResponsive
-                                content={
-                                    <Table hover>
-                                        <thead>
-                                            <tr>
-                                                {
-                                                    thArray.map((prop, key) => {
-                                                        return (
-                                                        <th  key={key}>{prop}</th>
-                                                        );
-                                                    })
-                                                }
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            {
-                                                tdArray.map((prop,key) => {
-                                                    return (
-                                                        <tr key={key}>{
-                                                            prop.map((prop,key)=> {
-                                                                return (
-                                                                    <td  key={key}>{prop}</td>
-                                                                );
-                                                            })
-                                                        }</tr>
-                                                    )
-                                                })
-                                            }
-                                        </tbody>
-                                    </Table>
-                                }
-                            />
-                        </Col>
+                        {/*<Col md={12}>*/}
+                            {/*<Card*/}
+                                {/*plain*/}
+                                {/*title="Air Handler Unit Data"*/}
+                                {/*category="AHU Data"*/}
+                                {/*ctTableFullWidth ctTableResponsive*/}
+                                {/*content={*/}
+                                    {/*<Table hover>*/}
+                                        {/*<thead>*/}
+                                            {/*<tr>*/}
+                                                {/*{*/}
+                                                    {/*thArray.map((prop, key) => {*/}
+                                                        {/*return (*/}
+                                                        {/*<th  key={key}>{prop}</th>*/}
+                                                        {/*);*/}
+                                                    {/*})*/}
+                                                {/*}*/}
+                                            {/*</tr>*/}
+                                        {/*</thead>*/}
+                                        {/*<tbody>*/}
+                                            {/*{*/}
+                                                {/*tdArray.map((prop,key) => {*/}
+                                                    {/*return (*/}
+                                                        {/*<tr key={key}>{*/}
+                                                            {/*prop.map((prop,key)=> {*/}
+                                                                {/*return (*/}
+                                                                    {/*<td  key={key}>{prop}</td>*/}
+                                                                {/*);*/}
+                                                            {/*})*/}
+                                                        {/*}</tr>*/}
+                                                    {/*)*/}
+                                                {/*})*/}
+                                            {/*}*/}
+                                        {/*</tbody>*/}
+                                    {/*</Table>*/}
+                                {/*}*/}
+                            {/*/>*/}
+                        {/*</Col>*/}
 
                     </Row>
                 </Grid>
