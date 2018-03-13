9b609ef68f8831cb4ac15a32b12d14de30e9aefd
Yepu Xie <12604828+xyreryp@users.noreply.github.com>
2018-03-13 00:06:33 -0700

    add equiment numbers & types

--- frontend/src/components/SearchBar/EquipmentNumber.js
+++ frontend/src/components/SearchBar/EquipmentNumber.js
@@ -1,7 +1,53 @@
 exports.EquipmentNumbers = [
-    { value: 'Silo value', label: 'Baseline_Modeled_Electricity', className: 'Building name' },
-    { value: 'Kemper Hall value', label: 'Baseline_Modeled_Electricity_MonthlyUsage', className: 'Building name' },
-    { value: 'MU value', label: 'Electricity_AnnualUsage', className: 'Building name' },
-    { value: 'Giedt value', label: 'Electricity_Demand_kBtu', className: 'Building name' },
-    { value: 'Rock Hall value', label: 'Electricity_EUI', className: 'Building name' },
+    {value: 'AHU01', label: 'AHU01', className: 'equipmentNumbers'},
+    {value: 'AHU02', label: 'AHU02', className: 'equipmentNumbers'},
+    {value: 'AHU03', label: 'AHU03', className: 'equipmentNumbers'},
+    {value: 'AHU04', label: 'AHU04', className: 'equipmentNumbers'},
+    {value: 'AHU05', label: 'AHU05', className: 'equipmentNumbers'},
+    {value: 'AHU06', label: 'AHU06', className: 'equipmentNumbers'},
+    {value: 'AHU07', label: 'AHU07', className: 'equipmentNumbers'}
 ];
+
+// exports.ACAD = [
+//     {value: 'AHU01', label: 'AHU01', className: 'Building name'},
+//     {value: 'AHU02', label: 'AHU02', className: 'Building name'},
+//     {value: 'AHU03', label: 'AHU03', className: 'Building name'},
+//     {value: 'AHU04', label: 'AHU04', className: 'Building name'},
+//     {value: 'AHU05', label: 'AHU05', className: 'Building name'},
+//     {value: 'AHU06', label: 'AHU06', className: 'Building name'},
+//     {value: 'AHU07', label: 'AHU07', className: 'Building name'},
+//     {value: 'AHU08', label: 'AHU08', className: 'Building name'}
+//
+// ];
+//
+// exports.AMRL = [
+//     {value: 'AHU01', label: 'AHU01', className: 'Building name'},
+// ];
+//
+// exports.ARC = [
+//     {value: 'AHU01', label: 'AHU01', className: 'Building name'},
+//     {value: 'AHU02', label: 'AHU02', className: 'Building name'},
+//     {value: 'AHU03', label: 'AHU03', className: 'Building name'},
+//     {value: 'AHU04', label: 'AHU04', className: 'Building name'},
+//     {value: 'AHU05', label: 'AHU05', className: 'Building name'},
+//     {value: 'AHU06', label: 'AHU06', className: 'Building name'},
+//     {value: 'AHU07', label: 'AHU07', className: 'Building name'}
+// ];
+//
+// exports.ARSM = [
+//     {value: 'AHU01', label: 'AHU01', className: 'Building name'},
+//     {value: 'AHU02', label: 'AHU02', className: 'Building name'}
+//
+// ];
+//
+// exports.ART = [
+//     {value: 'AHU01', label: 'AHU01', className: 'Building name'},
+//     {value: 'AHU02', label: 'AHU02', className: 'Building name'},
+//     {value: 'AHU03', label: 'AHU03', className: 'Building name'}
+//
+// ];
+//
+// exports.ARTX = [
+//     {value: 'AHU01', label: 'AHU01', className: 'Building name'},
+//     {value: 'AHU02', label: 'AHU02', className: 'Building name'}
+// ];
