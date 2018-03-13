879b6aecebc1a20a5d124d24202e6e98f10cf401
aidanbean <ajuengli@ucdavis.edu>
2018-03-12 17:40:13 -0700

    Configure ApolloClient in frontend/index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import registerServiceWorker from './registerServiceWorker'
+
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
 import {
     HashRouter,
     Route,
@@ -14,13 +21,21 @@ import './assets/sass/light-bootstrap-dashboard.css';
 import './assets/css/demo.css';
 import './assets/css/pe-icon-7-stroke.css';
 
+const httpLink = new HttpLink({ uri: `http://localhost:4000` })
 
-
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
 
 ReactDOM.render((
     <HashRouter>
         <Switch>
-            <Route path="/" name="Home" component={App}/>
+            <ApolloProvider client={client}>
+                <Route path="/" name="Home" component={App}/>
+            </ApolloProvider>
         </Switch>
     </HashRouter>
 ),document.getElementById('root'));
+
+// registerServiceWorker()
