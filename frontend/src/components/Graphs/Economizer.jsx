094715fa2ae3cc291d5bffdcd7ebe57049fd4144
aTonyXiao <tony.xiao95@icloud.com>
2018-05-31 00:00:43 -0700

    fix the timezone issue

--- frontend/src/components/Graphs/Economizer.jsx
+++ frontend/src/components/Graphs/Economizer.jsx
@@ -114,37 +114,37 @@ class EconGraph extends Component {
 
                 point["x"] = (props.data.selectBuilding[i + 1].stream[j].Value);
                 point["y"] = (props.data.selectBuilding[i].stream[j].Value);
-                point["Timestamp"] = moment((props.data.selectBuilding[i + 1].stream[j].Timestamp), "YYYY-MM-DD HH:mm");
+                point["Timestamp"] = DateTime.fromISO(props.data.selectBuilding[i + 1].stream[j].Timestamp);
                 points.push(point);
             }
 
-            if (month.length != 0 || day.length != 0 || weekday.length != 0 || hour.length != 0) {
+            if (month.size !== undefined || day.size !== undefined || weekday.size !== undefined || hour.size !== undefined) {
                 points = points.filter(obj => {// filter by month
-                    if (month.length == 0) {
+                    if (month.size === undefined) {
                         return true;
                     }
-                    let dateTime = DateTime.fromISO(obj.Timestamp, {zone: 'utc'});
-                    return month.has(dateTime.month.toString());
+                    return month.has(obj.Timestamp.month.toString());
                 }).filter(obj => {
-                    if (day.length == 0) {
+                    if (day.size === undefined) {
                         return true;
                     }
-                    let dateTime = DateTime.fromISO(obj.Timestamp, {zone: 'utc'});
-                    return day.has(dateTime.day.toString());
+                    return day.has(obj.Timestamp.day.toString());
                 }).filter(obj => {
-                    if (weekday.length == 0) {
+                    if (weekday.size === undefined) {
                         return true;
                     }
-                    let dateTime = DateTime.fromISO(obj.Timestamp, {zone: 'utc'});
-                    return weekday.has(dateTime.weekday.toString());
+                    return weekday.has(obj.Timestamp.weekday.toString());
                 }).filter(obj => {
-                    if (hour.length == 0) {
+                    if (hour.size === undefined) {
                         return true;
                     }
-                    let dateTime = DateTime.fromISO(obj.Timestamp, {zone: 'utc'});
-                    return hour.has(dateTime.hour.toString());
+                    return hour.has(obj.Timestamp.hour.toString());
                 });
             }
+
+            points.forEach(point => {
+                point.Timestamp = point.Timestamp.toFormat("yyyy-LLL-dd HH:mm:ss")
+            });
 
             // choose a color
             var color = colors[(i / 2) % 10];
